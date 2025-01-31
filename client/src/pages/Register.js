ae9f993d493dfed53f0db140229aaf076ccdbf25
Dev-wizzychat <102896441+Dev-wizzychat@users.noreply.github.com>
2025-01-31 18:40:29 +0530

    Your commit message

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,50 +6,63 @@ import Spinner from "../components/Spinner";
 import "../index.css";
 
 const Register = () => {
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-  const submitHandler = async (values) => {
-    try {
-      setLoading(true);
-      await axios.post("/users/register", values);
-      message.success("Registration Successful");
-      setLoading(false);
-      navigate("/login");
-    } catch (error) {
-      setLoading(false);
-      message.error("Something went wrong");
-    }
-  };
+    const submitHandler = async(values) => {
+        try {
+            setLoading(true);
+            await axios.post("http://localhost:8080/api/v1/users/register", values);
+            message.success("Registration Successful");
+            setLoading(false);
+            navigate("/login");
+        } catch (error) {
+            setLoading(false);
+            message.error("Something went wrong");
+        }
+    };
 
-  useEffect(() => {
-    if (localStorage.getItem("user")) {
-      navigate("/");
-    }
-  }, [navigate]);
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            navigate("/");
+        }
+    }, [navigate]);
 
-  return (
-    <div className="register-page">
-      <h1 className="page-heading">Expense Tracker</h1>
-      {loading && <Spinner />}
-      <Form layout="vertical" onFinish={submitHandler} className="register-form">
-        <h1>Register Form</h1>
-        <Form.Item label="Name" name="name">
-          <Input />
-        </Form.Item>
-        <Form.Item label="Email" name="email">
-          <Input type="email" />
-        </Form.Item>
-        <Form.Item label="Password" name="password">
-          <Input type="password" />
-        </Form.Item>
-        <button className="btn btn-primary">Register</button>
-        <Link to="/login" className="link">
-          Already Registered? Click here to Login
-        </Link>
-      </Form>
-    </div>
-  );
+    return ( <
+        div className = "register-page" >
+        <
+        h1 className = "page-heading" > Expense Tracker < /h1> { loading && < Spinner / > } <
+        Form layout = "vertical"
+        onFinish = { submitHandler }
+        className = "register-form" >
+        <
+        h1 > Register Form < /h1> <
+        Form.Item label = "Name"
+        name = "name" >
+        <
+        Input / >
+        <
+        /Form.Item> <
+        Form.Item label = "Email"
+        name = "email" >
+        <
+        Input type = "email" / >
+        <
+        /Form.Item> <
+        Form.Item label = "Password"
+        name = "password" >
+        <
+        Input type = "password" / >
+        <
+        /Form.Item> <
+        button className = "btn btn-primary" > Register < /button> <
+        Link to = "/login"
+        className = "link" >
+        Already Registered ? Click here to Login <
+        /Link> <
+        /Form> <
+        /div>
+    );
 };
 
 export default Register;
